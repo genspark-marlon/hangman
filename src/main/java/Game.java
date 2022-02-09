c00fe2fb3c1fd1c6cc346abfdce6977ed8fa3cd8
marlonaesparza <marlonaesparza@gmail.com>
2022-02-09 12:49:26 -0800

    Set up for building and testing.

---
+++ src/main/java/Game.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+class Game {
+  String randomWord;
+  int maxGuesses;
+  int guessCount;
+
+  public Game () {
+    this.maxGuesses = 8;
+    this.guessCount = 0;
+  }
+
+  public void incrementGuessCount () {
+    this.guessCount++;
+  }
+
+  public generateRandomWord () {
+    ArrayList<String> words = new ArrayList<String>();
+
+  }
+
+  public void initialize () {
+
+  }
+}
